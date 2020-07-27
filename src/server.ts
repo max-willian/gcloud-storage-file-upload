49031ba3f4945777afa98d98e135bc0e32cdce7c
Max W. Maluf <maxwillian63@gmail.com>
2020-07-27 01:43:21 -0300

    debug

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.listen(port, err => {
   if (err) {
     return console.error(err);
   }
-  return console.log(`server  listening on ${port}`);
+  return console.log(`maxx server listening on ${port}`);
 });
