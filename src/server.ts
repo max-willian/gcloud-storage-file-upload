624b28f00c14725c6c2e8ee99e60c30429cd7080
Max W. Maluf <maxwillian63@gmail.com>
2020-07-27 01:43:52 -0300

    debug

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.listen(port, err => {
   if (err) {
     return console.error(err);
   }
-  return console.log(`maxx server listening on ${port}`);
+  return console.log(`maxx server listening on ${port}. project id is ${process.env.PROJECT_ID}`);
 });
