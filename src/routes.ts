9540ca0a06f5db2b898e7efe3c541663c4d01e8a
Max W. Maluf <maxwillian63@gmail.com>
2020-07-29 02:14:20 -0300

    mais debug

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,6 @@ import PdfUploaderController from './controllers/PdfUploaderController';
 const router = express.Router();
 const pdfController = new PdfUploaderController();
 
-router.get('/', pdfController.test);
 router.get('/upload', pdfController.upload);
 router.get('/see', pdfController.get);
 
